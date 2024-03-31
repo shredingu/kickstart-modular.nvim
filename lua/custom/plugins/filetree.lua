e060e6bdf67fa68792a1dfe48bdafaf93e7732cf
A.Abelha <andre.abelha@siemens.com>
2024-03-31 19:40:52 +0100

    port lsp configs

--- lua/custom/plugins/filetree.lua
+++ lua/custom/plugins/filetree.lua
@@ -7,7 +7,20 @@ return {
     'MunifTanjim/nui.nvim',
     '3rd/image.nvim',
   },
-  config = function()
-    require('neo-tree').setup {}
-  end,
+  opts = {
+    filesystem = {
+      filtered_items = {
+        visible = true,
+        show_hidden_count = true,
+        hide_dotfiles = false,
+        hide_gitignored = true,
+        hide_by_name = {
+          -- '.git',
+          -- '.DS_Store',
+          -- 'thumbs.db',
+        },
+        never_show = {},
+      },
+    },
+  },
 }
