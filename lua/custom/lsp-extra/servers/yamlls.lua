1b326c527ca2e8ce73bc0c2508df0f3d6200d5f7
A.Abelha <andre.abelha@siemens.com>
2024-06-11 11:58:48 +0100

    de onde e' que isto vem?

--- lua/custom/lsp-extra/servers/yamlls.lua
+++ lua/custom/lsp-extra/servers/yamlls.lua
@@ -1,18 +1,21 @@
 return {
-    settings = {
-        yaml = {
-            format = {
-                enable = true,
-                singleQuote = true,
-                bracketSpacing = true,
-                proseWrap = "always",
-                printWidth = 80,
-            },
-            completion = true,
-            validate = true,
-            schemas = {
-                ["https://gitlab.com/gitlab-org/gitlab/-/raw/master/app/assets/javascripts/editor/schema/ci.json"] = "/*",
-            },
-        },
+  settings = {
+    yaml = {
+      format = {
+        enable = true,
+        singleQuote = true,
+        bracketSpacing = true,
+        proseWrap = 'always',
+        printWidth = 80,
+      },
+      completion = true,
+      validate = true,
+      schemaStore = {
+        enable = true,
+      },
+      schemas = {
+        ['https://gitlab.com/gitlab-org/gitlab/-/raw/master/app/assets/javascripts/editor/schema/ci.json'] = '/*',
+      },
     },
+  },
 }
